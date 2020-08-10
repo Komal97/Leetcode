be3ec8efcee8175c164e0a381023fad20691ef2e
Komal <komalbansal97@gmail.com>
2020-08-10 17:17:44 +0530

    min path sum in dp added

--- Dynamic Programming/min_path_sum.py
+++ Dynamic Programming/min_path_sum.py
@@ -1,4 +1,5 @@
 '''
+https://leetcode.com/problems/minimum-path-sum/
 Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
 Note: You can only move either down or right at any point in time.
 
@@ -13,24 +14,44 @@ Output: 7
 Explanation: Because the path 1→3→1→1→1 minimizes the sum.
 '''
 
+# recursive
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         
+        def findCost(grid, i, j, m, n):
+            
+            if i >= m or j >= n:
+                 return float('inf')
+            
+            if i == m-1 and j == n - 1:
+                return grid[i][j]
+            
+            
+            val1 = grid[i][j] + findCost(grid, i+1, j, m, n)
+            val2 = grid[i][j] + findCost(grid, i, j+1, m, n)
+            return min(val1, val2)
+    
+        return findCost(grid, 0, 0, len(grid), len(grid[0]))
+    
+# tabulation
+# if start from 0,0 => at a position, one can come from top or left
+# if start from m,n => from a position, one can go to down or right 
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
         
-        n = len(grid)
-        m = len(grid[0])
+        m = len(grid)
+        n = len(grid[0])
         
-        for i in range(n):
-            for j in range(m):
+        for i in range(m):
+            for j in range(n):
                 
-                if i-1<0 and j-1<0:
+                if i == 0 and j == 0:
                     continue
-                elif j-1 < 0:
+                if i == 0:
+                    grid[i][j] += grid[i][j-1] 
+                elif j == 0:
                     grid[i][j] += grid[i-1][j] 
-                elif i-1 < 0:
-                    grid[i][j] += grid[i][j-1]
                 else:
-                    grid[i][j] = min(grid[i][j] + grid[i][j-1], grid[i-1][j] + grid[i][j])
+                    grid[i][j] += min(grid[i-1][j], grid[i][j-1])
                     
-        
-        return grid[n-1][m-1]
+        return grid[m-1][n-1]
