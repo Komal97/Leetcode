a2de02bb53180041f478f3ec46769bc5be6c45a1
Komal <komalbansal97@gmail.com>
2020-11-03 00:33:49 +0530

    group anagram added

--- Hashing/group_anagrams.py
+++ Hashing/group_anagrams.py
@@ -1,4 +1,5 @@
 '''
+https://leetcode.com/problems/group-anagrams/
 Given an array of strings, group anagrams together.
 
 Example:
@@ -15,21 +16,41 @@ All inputs will be in lowercase.
 The order of your output does not matter.
 '''
 
+# method - 1 => since it requires sorted string as key, complexity = O(NKlog(K))
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]):
+            
+        group = defaultdict(list)
+        
+        for s in strs:    
+            string = ''.join(sorted(s))
+            group[string].append(s)
+        
+        res = []
+        for s in group:
+            res.append(group[s])
+        
+        return res
+
+# method - 2 => anagrams have same character count, so keep a count array and make that as key,
+# complexity = O(NK)
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
 
-        my_dict = {}
+        group = defaultdict(list)
         
-        for i in range(len(strs)):
-            key = ''.join(sorted(strs[i]))
-            if key not in my_dict:
-                my_dict[key] = []
-            my_dict[key].append(strs[i])
-            
-        strs = []
+        for s in strs:    
+            count = [0]*26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            #string = ''.join(sorted(s))
+            group[tuple(count)].append(s)
         
-        for key in my_dict:
-            strs.append(my_dict[key])
-            
-        return strs
+        res = []
+        for s in group:
+            res.append(group[s])
         
+        return res
