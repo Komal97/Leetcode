2a7987a0353b8c2cb15eb0dc573832f8107cfbf1
komal.bansal1 <komal.bansal@gxs.com.sg>
2024-07-18 20:20:11 +0530

    remove dupliacte character code updated

--- Stack/remove_duplicate_letter.py
+++ Stack/remove_duplicate_letter.py
@@ -11,33 +11,30 @@ Input: s = "cbacdcbc"
 Output: "acdb"
 '''
 
+from collections import Counter, defaultdict
 class Solution:
-    def removeDuplicateLetters(self, s: str):
-        
-        n = len(s)
-        if n == 0 or n == 1:
-            return s
+    def removeDuplicateLetters(self, s: str) -> str:
         
         # to keep track of prv processed elements
-        stack = []                             
+        stack = [] 
         # to keep character count, so that we can predict that current greater character is available in future or not
-        cnt = Counter(s)  
-        # to keep track of unique character visit               
-        visited = defaultdict(bool)
-        
+        cnt = Counter(s) 
+         # to keep track of unique character visit          
+        visited = defaultdict(bool)  
         for ch in s:
-            if visited[ch] == False:
-                # check if prev pushed element is greater
-                if len (stack) > 0 and stack[-1] > ch:  
-                    # pop and mark not visited if stack top is greater and is available in future to consume                    
-                    while len (stack) > 0 and stack[-1] > ch and cnt[stack[-1]] > 0:
-                        c = stack.pop()
-                        visited[c] = False
-                # finally push character in stack and mark visited
-                stack.append(ch)
-                visited[ch] = True
-            # always reduce character count to keep character count
+            # always reduce character count to keep character count for future characters
             cnt[ch] -= 1
+            # say, if b is coming at pos 0 and pos 9, then pos 0 will be considered as that can guarantee smaller lexicographical string and can be identified with visisted
+            if visited[ch] == True: 
+                continue
             
+            # check if prev pushed element is greater
+            # pop and mark not visited if stack top is greater and is available in future to consume           
+            while len(stack)>0 and ch < stack[-1] and cnt[stack[-1]] > 0:
+                prev = stack.pop()
+                visited[prev] = False
+            # finally push character in stack and mark visited
+            stack.append(ch)
+            visited[ch] = True
         
-        return ''.join(stack)
+        return ''.join(stack)    
